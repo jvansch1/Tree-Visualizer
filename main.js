ecd151270c084bf7e292dd1d47a582ccc8984367
John Van Schultz <jvansch1@binghamton.edu>
2017-06-14 18:58:57 -0400

    Set up Canvas and Sass

---
+++ main.js
@@ -0,0 +1,15 @@
+function main() {
+  document.addEventListener("DOMContentLoaded", () => {
+    let canvas = document.getElementById("main")
+    let ctx = canvas.getContext('2d')
+
+    canvas.height = document.body.clientHeight;
+    canvas.width = document.body.clientWidth;
+
+    ctx.beginPath();
+    // x, y, radius, start angle, end angle, counter-clockwise
+    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+    ctx.fillStyle = 'red';
+    ctx.fill();
+  })
+}
